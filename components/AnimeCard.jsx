2d61236639810612e77273d014701d0064d72b44
Kanisevsky <meltsmokestudio@gmail.com>
2024-02-12 18:31:32 +0000

    fetching the data on a server

--- components/AnimeCard.jsx
+++ components/AnimeCard.jsx
@@ -5,7 +5,7 @@ function AnimeCard({ anime }) {
     <div className="max-w-sm rounded relative w-full">
       <div className="relative w-full h-[37vh]">
         <Image
-          src={anime.image.original}
+          src={`https://shikimori.one${anime.image.original}`}
           alt={anime.name}
           fill
           className="rounded-xl"
